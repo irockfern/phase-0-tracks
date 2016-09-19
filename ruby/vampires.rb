da89dd6839d6eab5806d9c0c835c8a81d66a4152
Pierre-Allen Forney <iRockfern@Ians-MacBook-Pro.local>
2016-09-19 00:53:05 -0700

    added questions and user input

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "Ouy company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
