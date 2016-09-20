f0425afe49f6b7c0a8d22fb0b5295c9f94809f8d
Pierre-Allen Forney <iRockfern@Ians-MacBook-Pro.local>
2016-09-20 12:18:14 -0700

    updated code to reflect release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,5 @@
+puts "How many employees will be processed?"
+processed = gets.chomp.to_i
 
 puts "What is your name?"
 name = gets.chomp
@@ -8,8 +10,29 @@ age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
 
-puts "Ouy company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
+
+age_verify = 2016 - birth_year
+
+if age == age_verify
+	age = TRUE
+else
+	age = FALSE
+end
+
+if age && (garlic_bread || health_insurance)
+	puts "Probably not a vampire."
+elsif !age && ( !garlic_bread || !health_insurance)
+	puts "Probably a vampire."
+elsif !age && !garlic_bread && !health_insurance
+	puts "Almost certainly a vampire."
+elsif name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+
