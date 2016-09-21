248f1d99c70f34e524d96d043388acc918afaecf
Pierre-Allen Forney <iRockfern@Ians-MacBook-Pro.local>
2016-09-21 09:24:37 -0700

    added additional loop for allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,25 +12,49 @@ while processed > 0
 	puts "What year were you born?"
 	birth_year = gets.chomp.to_i
 	
-	puts "Our company cafeteria serves garlic 	bread. Should we order some for you?"
-	garlic_bread = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp.downcase
 	
-	puts "Would you like to enroll in the 	company's health insurance?"
-	health_insurance = gets.chomp
+	puts "Would you like to enroll in the company's health insurance?"
+	health_insurance = gets.chomp.downcase
+
+	puts "List allergies one at a time and type done when finished"
+	allergies = gets.chomp.downcase
+
+	while allergies != "done" && allergies != "sunshine"
+		puts "Next allergy:"
+		allergies = gets.chomp.downcase
 	
+		if allergies == "sunshine"
+			puts "Probably a vampire."
+		end
+	end
+
 	age_verify = 2016 - birth_year
 	
 	if age == age_verify
-		age = TRUE
+		age = true
 	else
-		age = FALSE
+		age = false
 	end
 	
+	if garlic_bread == "yes"
+		garlic_bread = true
+	else
+		garlic_bread = false
+	end
+
+	if health_insurance == "yes"
+		health_insurance = true
+	else
+		health_insurance = false
+	end
+
 	if age && (garlic_bread || health_insurance	)
 		puts "Probably not a vampire."
-	elsif !age && ( !garlic_bread || 	!health_insurance)
+	elsif !age && ( !garlic_bread || !health_insurance)
 		puts "Probably a vampire."
-	elsif !age && !garlic_bread && 	!health_insurance
+	elsif !age && ( !garlic_bread && !health_insurance)
 		puts "Almost certainly a vampire."
 	elsif name == "Drake Cula" || name == "Tu 	Fang"
 		puts "Definitely a vampire."
@@ -38,3 +62,5 @@ while processed > 0
 		puts "Results inconclusive."
 	end
 end
+end
+
