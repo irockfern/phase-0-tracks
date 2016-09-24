3c883daefffc7503a024553dbf868d5a01ef056c
Pierre-Allen Forney <iRockfern@ians-mbp.attlocal.net>
2016-09-24 11:58:20 -0700

    created initial hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,2 +1,19 @@
 #name, age, #children, decor theme, dog?, cat?
 
+#prompt user for information
+#convert input to appropriate data type
+#print hash to screen
+#update information
+	#if "none" skip
+	#elsif {key} update
+#print updated information
+
+client = {
+	name:
+	age:
+	"# of children" =>
+	decor:
+	dog:
+	cat:
+}
+
