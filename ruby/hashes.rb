8661558071e551702a55b24f666e02182d07f4c1
Pierre-Allen Forney <iRockfern@ians-mbp.attlocal.net>
2016-09-24 12:58:31 -0700

    completed

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -17,33 +17,52 @@ client = {
 	cats:"nil",
 }
 
-puts "Client Name:"
-client_name = gets.chomp
-insert_name = client[:name] = client_name
+def client_info(client)
+	puts "Client Name:"
+	client_name = gets.chomp
+	insert_name = client[:name] = client_name
+	
+	puts "Client Age:"
+	client_age = gets.chomp.to_i
+	insert_age = client[:age] = client_age
+	
+	puts "# of Children:"
+	client_children = gets.chomp.to_i
+	insert_children = client[:children] = client_children
+	
+	puts "Decor Theme:"
+	client_decor = gets.chomp
+	insert_decor = client[:decor] = client_decor
+	
+	puts "Does client have dogs?"
+	client_dogs = gets.chomp
+	insert_dogs = client[:dogs] = client_dogs
+	
+	puts "Does client have cats?"
+	client_cats = gets.chomp
+	insert_cats = client[:cats] = client_cats
+	
+	puts "Name: #{client[:name]}"
+	puts "Age: #{client[:age]}"
+	puts "# of Children: #{client[:children]}"
+	puts "Decor Theme: #{client[:decor]}"
+	puts "Dogs: #{client[:dogs]}"
+	puts "Cats: #{client[:cats]}"
+end
 
-puts "Client Age:"
-client_age = gets.chomp.to_i
-insert_age = client[:age] = client_age
+puts "Add a new client?"
+new_client = gets.chomp.downcase
+if new_client == "yes"
+	client_info(client)
+else
+	puts "All done!"
+end
 
-puts "# of Children:"
-client_children = gets.chomp.to_i
-insert_children = client[:children] = client_children
+puts "Do you need to make changes?"
+changes = gets.chomp.downcase
 
-puts "Decor Theme:"
-client_decor = gets.chomp
-insert_decor = client[:decor] = client_decor
-
-puts "Does client have dogs?"
-client_dogs = gets.chomp
-insert_dogs = client[:dogs] = client_dogs
-
-puts "Does client have cats?"
-client_cats = gets.chomp
-insert_cats = client[:cats] = client_cats
-
-puts "Name: #{client[:name]}"
-puts "Age: #{client[:age]}"
-puts "# of Children: #{client[:children]}"
-puts "Decor Theme: #{client[:decor]}"
-puts "Dogs: #{client[:dogs]}"
-puts "Cats: #{client[:cats]}"
+if changes == "yes"
+	client_info(client)
+else
+	puts "All done!"
+end
