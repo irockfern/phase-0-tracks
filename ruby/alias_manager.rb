f162e27b08bfaf5ebcac173bd622bb4082c07036
Pierre-Allen Forney <iRockfern@Ians-MacBook-Pro.local>
2016-09-26 11:27:24 -0700

    split & reversed name entry | split vowel & consonant hash

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,1 +1,16 @@
-#hello world
+#swap first and last name
+puts "Enter Name:"
+spy_name = gets.chomp.split(' ').reverse
+
+
+# .next vowels
+# .next consonants
+=begin
+letters = {
+	vowels:'aeiou',
+	consonants:'bcdfghjklmnpqrstvwxyz',
+}
+
+letters[:vowels].split('')
+letters[:consonants].split('')
+=end
